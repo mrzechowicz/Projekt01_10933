60979095f8e4dfcb134a15e79803a9a9cd62e7b9
MR <m.rzechowicz@gmail.com>
2018-10-07 15:03:57 +0200

    tabela produkt

---
+++ Projekt01DB/Projekt01DB/dbo/Tables/PRODUKT.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[PRODUKT] (
+    [Prd_Id]          INT             IDENTITY (1, 1) NOT NULL,
+    [Prd_ProducentId] INT             NULL,
+    [Prd_Nazwa]       VARCHAR (100)   NULL,
+    [Prd_Kolor]       VARCHAR (100)   NULL,
+    [Prd_CenaNetto]   DECIMAL (18, 2) NULL,
+    [Prd_CenaBrutto]  DECIMAL (18, 2) NULL,
+    PRIMARY KEY CLUSTERED ([Prd_Id] ASC),
+    CONSTRAINT [FK_ProducentId] FOREIGN KEY ([Prd_ProducentId]) REFERENCES [dbo].[PRODUCENT] ([Prc_Id]) ON DELETE SET NULL ON UPDATE SET NULL
+);
+
