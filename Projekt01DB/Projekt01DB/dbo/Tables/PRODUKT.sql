72b7753523762a7a7cc14110473f539518d35bcd
MR <m.rzechowicz@gmail.com>
2018-10-07 15:07:48 +0200

    dodanie 2 kolumn do tabeli PRODUKT (insertdate/updatedate)

--- Projekt01DB/Projekt01DB/dbo/Tables/PRODUKT.sql
+++ Projekt01DB/Projekt01DB/dbo/Tables/PRODUKT.sql
@@ -5,7 +5,11 @@
     [Prd_Kolor]       VARCHAR (100)   NULL,
     [Prd_CenaNetto]   DECIMAL (18, 2) NULL,
     [Prd_CenaBrutto]  DECIMAL (18, 2) NULL,
+    [Prd_InsertDate]  DATETIME        DEFAULT (getdate()) NULL,
+    [Prd_UpdateDate]  DATETIME        NULL,
     PRIMARY KEY CLUSTERED ([Prd_Id] ASC),
     CONSTRAINT [FK_ProducentId] FOREIGN KEY ([Prd_ProducentId]) REFERENCES [dbo].[PRODUCENT] ([Prc_Id]) ON DELETE SET NULL ON UPDATE SET NULL
 );
+
+
 
